af9134753307f72e3ccece8c45def638449bf7ab
Alfario Eka Putra <alfarioekaputra@gmail.com>
2019-08-13 23:00:10 +0700

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
+    title: 'M Kasir',
     home: Home(),
   ));
 }
@@ -10,20 +11,10 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: 
-        Center(
-          child: Container(
-            color: Colors.green[600],
-            width: 200.0,
-            height: 100.0,
-            child: Center(
-              child: Text(
-                'Flutter tutorial',
-                style: TextStyle(color: Colors.white, fontFamily: "Roboto", fontSize: 20),
-              ),
-            ),
-          )
-        )
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Dashboard'),
+      ),
 
     );
   }
